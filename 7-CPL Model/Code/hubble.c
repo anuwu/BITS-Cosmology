b3f1a9009e23bfdf313aeb26fe99b150354696cd
anwesh0304 <anwesh0304@gmail.com>
2020-06-14 17:47:59 +0530

    Corrected error in 7-CPL model

--- 7-CPL Model/Code/hubble.c
+++ 7-CPL Model/Code/hubble.c
@@ -17,6 +17,6 @@ double hub (double z)
 	+
 	om_r0*pow((1+z),4)
 	+
-	(1 - om_m0 - om_r0)*exp(3*(w0+wa+1)*log(z+1) - wa*z*(1089*pow(z,6) + 4683*pow(z,5) + 96939*pow(z,4) + 11165*pow(z,3) + 7490*pow(z,2) + 2730*z + 420)/(140 * pow((z+1),7)))
+	(1 - om_m0 - om_r0)*exp(3*(w0+wa+1)*log(z+1) - wa*z*(1089*pow(z,6) + 4683*pow(z,5) + 9639*pow(z,4) + 11165*pow(z,3) + 7490*pow(z,2) + 2730*z + 420)/(140 * pow((z+1),7)))
 	) ;
 }
